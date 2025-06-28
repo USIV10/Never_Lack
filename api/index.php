3eaaf6579cc9a33446520bf6b326ab35716de2ea
USIV10 <usivabdulkarim@gmail.com>
2025-06-28 22:39:10 +0000

    Updated layout and fixed asset paths for Vercel

--- api/index.php
+++ api/index.php
@@ -18,15 +18,15 @@
       <meta name="description" content="">
       <meta name="author" content="">
       <!-- bootstrap css -->
-      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="/css/bootstrap.min.css">
       <!-- style css -->
-      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="/css/style.css">
       <!-- Responsive-->
-      <link rel="stylesheet" href="css/responsive.css">
+      <link rel="stylesheet" href="/css/responsive.css">
       <!-- fevicon -->
-      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <link rel="icon" href="/images/fevicon.png" type="image/gif" />
       <!-- Tweaks for older IEs-->
-      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="/https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
       <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
@@ -35,14 +35,14 @@
    <body class="main-layout">
       <!-- loader  -->
       <div class="loader_bg">
-         <div class="loader"><img src="images/loading.gif" alt="#"/></div>
+         <div class="loader"><img src="/images/loading.gif" alt="#"/></div>
       </div>
       <!-- end loader -->
       <!-- header -->
       <header>
          <div class="menu_sitbar di_mr01">
             <div class="logo">
-               <a href="index.php"><img src="images/logo.png" alt="#"/></a>
+               <a href="/index.php"><img src="/images/logo.png" alt="#"/></a>
             </div>
             <nav class="navigation navbar navbar-expand-md navbar-dark ">
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
@@ -51,16 +51,16 @@
                <div class="collapse navbar-collapse" id="navbarsExample05">
                   <ul class="navbar-nav mr-auto">
                      <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home</a>
+                        <a class="nav-link" href="/index.php">Home</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="shop.php">Shop</a>
+                        <a class="nav-link" href="/shop.php">Shop</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact Us</a>
+                        <a class="nav-link" href="/contact.php">Contact Us</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="cart.php">Cart</a>
+                        <a class="nav-link" href="/cart.php">Cart</a>
                      </li>
                   </ul>
                </div>
@@ -74,7 +74,7 @@
                         <div class="full">
                            <div class="center-desk">
                               <div class="logo">
-                                 <a href="index.php"><img src="images/logo.png" alt="#"/></a>
+                                 <a href="/index.php"><img src="/images/logo.png" alt="#"/></a>
                               </div>
                            </div>
                         </div>
@@ -87,19 +87,19 @@
                            <div class="collapse navbar-collapse" id="navbarsExample04">
                               <ul class="navbar-nav mr-auto">
                                  <li class="nav-item active">
-                                    <a class="nav-link" href="index.php">Home</a>
+                                    <a class="nav-link" href="/index.php">Home</a>
                                  </li>
                                  <li class="nav-item">
-                                    <a class="nav-link" href="shop.php">Shop</a>
+                                    <a class="nav-link" href="/shop.php">Shop</a>
                                  </li>
                                  <li class="nav-item">
-                                    <a class="nav-link" href="about.php">About</a>
+                                    <a class="nav-link" href="/about.php">About</a>
                                  </li>
                                  <li class="nav-item">
                                     <a class="nav-link" href="contact.php">Contact Us</a>
                                  </li>
                                  <li class="nav-item">
-                                    <a class="nav-link" href="cart.php">Cart</a>
+                                    <a class="nav-link" href="/cart.php">Cart</a>
                                  </li>
                               </ul>
                            </div>
@@ -114,19 +114,19 @@
                         <span class="nav-link">Welcome, <?= htmlspecialchars($username); ?></span>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Logout</a>
+                        <a class="nav-link" href="/logout.php">Logout</a>
                      </li>
                   </ul>
                <?php else: ?>
                   <ul class="navbar-nav ml-auto">
                      <li class="nav-item">
-                        <a class="nav-link" href="register_login.php"></a>
+                        <a class="nav-link" href="/register_login.php"></a>
                      </li>
                <?php endif; ?>
                      </div>
                      <div class="col-md-4">
                         <ul class="email text_align_right">
-                           <li class="d_none"><a href="register_login.php"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+                           <li class="d_none"><a href="/register_login.php"><i class="fa fa-user" aria-hidden="true"></i></a></li>
                            <li class="d_none"> <a href="Javascript:void(0)"><i class="fa fa-search" style="cursor: pointer;" aria-hidden="true"></i></a> </li>
                         </ul>
                      </div>
@@ -160,12 +160,12 @@
                                                    Neat peanut and Peanut Butter Here
                                                 </h3>
                                                 <p>Handcrafted with love, roasted to perfection</p>
-                                                <a class="read_more" href="contact.php">Contact  </a>
+                                                <a class="read_more" href="/contact.php">Contact  </a>
                                              </div>
                                           </div>
                                           <div class="col-md-5">
                                              <div class="banner_img">
-                                                <figure><img class="img_responsive" src="images/banner_img.png" style="border-radius: 35px;"></figure>
+                                                <figure><img class="img_responsive" src="/images/banner_img.png" style="border-radius: 35px;"></figure>
                                              </div>
                                           </div>
                                        </div>
@@ -181,12 +181,12 @@
                                                   Elevate your snacking experience with our classy peanuts
                                                 </h3>
                                                 <p>Crunch your way to peanut paradise</p>
-                                                <a class="read_more" href="contact.php">Contact  </a>
+                                                <a class="read_more" href="/contact.php">Contact  </a>
                                              </div>
                                           </div>
                                           <div class="col-md-5">
                                              <div class="banner_img">
-                                                <figure><img class="img_responsive" src="images/banner_img.png" style="border-radius: 35px;"></figure>
+                                                <figure><img class="img_responsive" src="/images/banner_img.png" style="border-radius: 35px;"></figure>
                                              </div>
                                           </div>
                                        </div>
@@ -202,12 +202,12 @@
                                                    Crunchy bliss in every bite
                                                 </h3>
                                                 <p>Taste our delicious peanuts</p>
-                                                <a class="read_more" href="contact.php">Contact  </a>
+                                                <a class="read_more" href="/contact.php">Contact  </a>
                                              </div>
                                           </div>
                                           <div class="col-md-5">
                                              <div class="banner_img">
-                                                <figure><img class="img_responsive" src="images/banner_img.png" style="border-radius: 35px;"></figure>
+                                                <figure><img class="img_responsive" src="/images/banner_img.png" style="border-radius: 35px;"></figure>
                                              </div>
                                           </div>
                                        </div>
@@ -240,12 +240,12 @@
                   <div class="titlepage text_align_left">
                      <h2>About Us</h2>
                      <p></p>
-                     <a class="read_more" href="about.php">Read More</a>
+                     <a class="read_more" href="/about.php">Read More</a>
                   </div>
                </div>
                <div class="col-md-6">
                   <div class="about_img text_align_center">
-                     <figure><img class="img_responsive" src="images/about.png" alt="#"/></figure>
+                     <figure><img class="img_responsive" src="/images/about.png" alt="#"/></figure>
                   </div>
                </div>
             </div>
@@ -271,37 +271,37 @@
                </div>
                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                   <div class="quality-box ">
-                     <figure><img src="images/quality2.png" alt="#"/></figure>
+                     <figure><img src="/images/quality2.png" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                   <div class="quality-box ">
-                     <figure><img src="images/quality3.png" alt="#"/></figure>
+                     <figure><img src="/images/quality3.png" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                   <div class="quality-box ">
-                     <figure><img src="images/quality4.png" alt="#"/></figure>
+                     <figure><img src="/images/quality4.png" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                   <div class="quality-box ">
-                     <figure><img src="images/quality5.png" alt="#"/></figure>
+                     <figure><img src="/images/quality5.png" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                   <div class="quality-box ">
-                     <figure><img src="images/quality6.png" alt="#"/></figure>
+                     <figure><img src="/images/quality6.png" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                   <div class="quality-box ">
-                     <figure><img src="images/quality7.png" alt="#"/></figure>
+                     <figure><img src="/images/quality7.png" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6">
                   <div class="quality-box ">
-                     <figure><img src="images/quality8.png" alt="#"/></figure>
+                     <figure><img src="/images/quality8.png" alt="#"/></figure>
                   </div>
                </div>
                <div class="col-sm-12">
@@ -376,10 +376,10 @@
       </footer>
       <!-- end footer -->
       <!-- Javascript files-->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="/js/jquery.min.js"></script>
+      <script src="/js/bootstrap.bundle.min.js"></script>
+      <script src="/js/jquery-3.0.0.min.js"></script>
       <!-- sidebar -->
-      <script src="js/custom.js"></script>
+      <script src="/js/custom.js"></script>
 </body>
 </html>
